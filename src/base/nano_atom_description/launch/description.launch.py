4fd10fe08f76c0d33eb9a40a205889c8528b7171
Robert Vasquez Zavaleta <roboticarts1@gmail.com>
2025-09-27 17:33:31 +0200

    fix: Add required copyright header for linting (#1)

--- src/base/nano_atom_description/launch/description.launch.py
+++ src/base/nano_atom_description/launch/description.launch.py
@@ -1,22 +1,19 @@
 # Copyright (C) 2025, Robotic Arts
 #
-# This file is part of nano_atom.
-#
-# nano_atom is free software: you can redistribute it and/or modify
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# nano_atom is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with nano_atom.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 # Author: Robert Vasquez Zavaleta
-
 
 from launch_ros.actions import Node
 from launch import LaunchDescription
