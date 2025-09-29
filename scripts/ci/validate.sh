e28825b4187b543f22f93dfe481d8ea973f39dd7
Robert Vasquez Zavaleta <roboticarts1@gmail.com>
2025-09-29 20:59:34 +0200

    ci: Replace ament_copyright with licensecheck to run static analysis before build

--- scripts/ci/validate.sh
+++ scripts/ci/validate.sh
@@ -7,11 +7,12 @@ SCRIPT_DIR="$(realpath "$(dirname "$0")/../../src")"
 echo $SCRIPT_DIR
 pushd "$SCRIPT_DIR" > /dev/null
 
-if ament_copyright --verbose; then
-  echo "✅ PASSED: Copyright"
+# if ament_copyright --verbose; then
+if ! licensecheck --machine --copyright $(find . -name '*launch.py') | grep -E "UNKNOWN|No copyright"; then
+  echo "✅ PASSED: Launch copyright"
   exit_code=0
 else
-  echo "❌ FAILED: Copyright"
+  echo "❌ FAILED: Launch copyright"
   exit_code=1
 fi
 
